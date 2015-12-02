5cd8d52672756e8592f83685ae9048b7cf404deb
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-12-02 21:16:12 +0000

    fitting coefficients computed

--- pmode/cutoff/poly.m
+++ pmode/cutoff/poly.m
@@ -1,8 +1,8 @@
 function [ y ] = poly(cof, x  )
 %Compute polynomial from 1 to 9
 %   cof is structure with coefficients from p1 to p9
-
-y=cof.p9+cof.p8.*x+cof.p7.*x.^2+cof.p6.*x.^3+cof.p5.*x.^4+cof.p4.*x.^5+cof.p3.*x.^6+cof.p2.*x.^7+cof.p1.*x.^8;
+%x=x./meanx;
+y=cof.p10+cof.p9.*x+cof.p8.*(x.^2)+cof.p7.*(x.^3)+cof.p6.*(x.^4)+cof.p5.*(x.^5)+cof.p4.*(x.^6)+cof.p3.*(x.^7)+cof.p2.*(x.^8)+cof.p1.*(x.^9);
 
 end
 
