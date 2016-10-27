e9259f00987a3b028f3d2ff58d26b31c3664a8cc
mikeg64 <m.griffiths@sheffield.ac.uk>
2016-10-27 17:45:40 +0100

    updated klein-gordon atmosphere model

--- matlab/solatmoswaves/kg.m
+++ matlab/solatmoswaves/kg.m
@@ -4,7 +4,8 @@
 
 loadatmos;
 
-
+num=size(temp);
+nr=num(1);
 tri=1419; %index for position in transition layer (between 1324:1419)
 TP=180; %driver period in seconds
 om=2*pi/TP;
@@ -36,11 +37,41 @@ kgc.k1=k;
 % A1=(iom*bessely(kgc.nu,kgc.beta)-A2*(cos(k*kgc.L)*bessely(kgc.nu,kgc.alpha))/(sqrt(1*kgc.a*kgc.L)))*(besseli(kgc.nu,kgc.alpha)*bessely(kgc.nu,kgc.beta)-besseli(kgc.nu,kgc.beta)*bessely(kgc.nu,kgc.alpha));
 % B1=(iom-A1*besseli(kgc.nu,kgc.alpha))/bessely(kgc.nu,kgc.alpha);
 
-A2=iom*kgc.k1*exp(-j*kgc.k2*kgc.L)/(exp(j*kgc.k1*kgc.L)-sin(kgc.k1*kgc.L)*(j*kgc.k1+j*kgc.k2+((1/(2*kgc.lam2))-(1/(2*kgc.lam1)))));
+A2=iom*kgc.k1*exp(-1i*kgc.k2*kgc.L)/(exp(1i*kgc.k1*kgc.L)-sin(kgc.k1*kgc.L)*(1i*kgc.k1+1i*kgc.k2+((1/(2*kgc.lam2))-(1/(2*kgc.lam1)))));
+A1=(iom-A2*(exp(1i*(kgc.k1+kgc.k2)*kgc.L)))/(1-exp(2*1i*kgc.k1*kgc.L));
+B1=iom-A1;
+B2=0;
 
+%compute the Q
 
-
-
+for i=nr:-1:1 
+    csn=cs(i);
+    lamn=csn.^2./(consts.fgamma*consts.ggg);
+    omn=csn./(2.*lamn);
+    
+    if iom<omn
+        kn=(-1i).*sqrt(omn.*omn-iom.*iom)./csn;
+    end
+    
+    if iom>=omn
+        kn=sqrt(iom.*iom-omn.*omn)./csn;        
+    end   
+    
+    if nr>tri   %region1
+        A2=iom*kgc.k1*exp(-1i*kgc.k2*kgc.L)/(exp(1i*kgc.k1*kgc.L)-sin(kgc.k1*kgc.L)*(1i*kgc.k1+1i*kgc.k2+((1/(2*kgc.lam2))-(1/(2*kgc.lam1)))));
+        A1=(iom-A2*(exp(1i*(kgc.k1+kgc.k2)*kgc.L)))/(1-exp(2*1i*kgc.k1*kgc.L));
+        B1=iom-A1;
+        B2=0;
+        s(i)=-iom*kn*real((A1.^2-B1.^2));
+    end
+    if nr<=tri   %region 2 
+        A2=iom*kgc.k1*exp(-1i*kgc.k2*kgc.L)/(exp(1i*kgc.k1*kgc.L)-sin(kgc.k1*kgc.L)*(1i*kgc.k1+1i*kgc.k2+((1/(2*kgc.lam2))-(1/(2*kgc.lam1)))));
+        A1=(iom-A2*(exp(1i*(kgc.k1+kgc.k2)*kgc.L)))/(1-exp(2*1i*kgc.k1*kgc.L));
+        B1=iom-A1;
+        B2=0;
+        s(i)=-iom*kn*real((A2.^2-B2.^2));
+    end
+end
 
 
 
