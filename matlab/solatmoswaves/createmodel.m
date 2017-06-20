62a258cf4bcc33b6c9b2c12076f564c1a2c48021
mikeg64 <m.griffiths@sheffield.ac.uk>
2017-06-20 08:03:20 +0100

    Update to createmodel read VALIIIc data for upto 6Mm interpolate to create model. Use hydrstatic pressure balance to compute pressure and correct energy at different heights

--- matlab/solatmoswaves/createmodel.m
+++ matlab/solatmoswaves/createmodel.m
@@ -130,16 +130,249 @@ densg(nint-1)=densg(nint-2);
 %set background energy
 
 
-%e=p/(rho*(gamma-1.d0))+0.5d0*(bx*bx+bz*bz)
+%rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
+%set background density
+%set background energy
+mu=0.6d0;
+R=8.31e3;
+
+%parrVALMc=rhoarrVALMc*TarrVALMc*R/mu
+%rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
+% p[*,*,*]=w[*,*,*,4]+w[*,*,*,8]
+% p[*,*,*]=p[*,*,*]-(w[*,*,*,1]^2.0+w[*,*,*,2]^2.0+w[*,*,*,3]^2.0)/ $
+%          (w[*,*,*,0]+w[*,*,*,9])/2.0
+% p[*,*,*]=p[*,*,*]-((w[*,*,*,5]+w[*,*,*,10])^2.0+(w[*,*,*,6]+w[*,*,*,11])^2.0 $
+%           +(w[*,*,*,7]+w[*,*,*,12])^2.0)/2.0
+% p[*,*,*]=(gamma-1.d0)*p[*,*,*]
+
+
+%compute correct pressure for gravitationally stratified atmosphere
+
+%compute initial energy (at photosphere or temperature minimum)
+%mu_thermal=0.6d0;
+%R=8.31e3;
+
+% pressure=temp*R*density/((mu_thermal))
+%parrVALMc=rhoarrVALMc*TarrVALMc*R/mu
+%iniene=6840.d0*8.31e3*(2.3409724e-09)/0.6d0/(eqpar(gamma_)-1.0)
+
+% !iniene=731191.34d0*8.31e3*(1.1806882e-11)/0.6d0/(eqpar(gamma_)-1.0)
+% 
+% !iniene=731191.34d0*8.31e3*(1.1790001e-11)/0.6d0/(eqpar(gamma_)-1.0)
+% 
+% ! 1.6Mm
+% 
+iniene=6840.d0*R*(2.3409724e-09)/mu/(consts.fgamma-1.0);
+% 
+% !iniene=6840.d0*8.31e3*(2.2139002e-09)/0.6d0/(eqpar(gamma_)-1.0)
+% 
+% !iniene=731191.34d0*8.31e3*(4.5335481e-12)/0.6d0/(eqpar(gamma_)-1.0)
+
+
+%inix and inirho read from height versus density dta from valiiic mcwhirter
+%data
+%  do ix_2=ixGlo2,ixGhi2
+%   do ix_3=ixGlo3,ixGhi3 
+% 
+%    x(ix_1,ix_2,ix_3,1)=inix !*1000.d0
+%    w(ix_1,ix_2,ix_3,rho_)=inirho
+%    w(ix_1,ix_2,ix_3,e_)=iniene
+%    w(ix_1,ix_2,ix_3,m1_)=0.0
+%    w(ix_1,ix_2,ix_3,m2_)=0.0
+%    w(ix_1,ix_2,ix_3,m3_)=0.0   
+% 
+%   enddo
+%  enddo
+
 
       for i=1:nx1
            for j=1:nx2
                for k=1:nx3
                    simdata.w(i,j,k,10)=densg(i);  %density
-                   simdata.w(i,j,k,9)=presg(i)./(densg(i)*(consts.fgamma-1));
+                   simdata.w(i,j,k,9)=iniene;  %energy
+                                    
                end
            end
-       end
+      end
+
+
+
+% use energy to get pthermal
+
+% p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=half*( w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,m1_)**2+w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,m2_)**2+w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,m3_)&
+%    **2 )/(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,rho_)&
+%    +w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,rhob_))
+% 
+% p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=p(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3)+ half*( (w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_)&
+%    **2)+(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b2_)**2)&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)**2) )&
+%    +( (w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg1_))+(w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,b2_)*w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,bg2_))&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg3_)) )
+% 
+% p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=(eqpar(gamma_)&
+%    -one)*(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,e_)-p(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3))
+for i=1:nx1
+    presg(i)=(consts.fgamma-1)*iniene;
+end
+
+presg1=presg;
+
+
+
+%compute correct pressure for gravitationally stratified atmosphere
+
+% do ix_3=ixGlo3,ixGhi3
+%  do ix_2=ixGlo2,ixGhi2
+%   do ix_1=ixGhi1-1,ixGlo1,-1 
+% 
+% comi=-abs(x(ix_1+1,ix_2,ix_3,1)-x(ix_1,ix_2,ix_3,1))
+% 
+% w(ix_1,ix_2,ix_3,p_)=w(ix_1+1,ix_2,ix_3,p_)+w(ix_1,ix_2,ix_3,rho_)*comi*1.d0&
+%    *eqpar(grav1_)
+% 
+% 
+% 
+%   enddo
+%  enddo
+% enddo
+
+
+ 
+
+
+for i=nx1-1:-1:1
+    comi=-abs(rheight(i+1)-rheight(i));
+    presg(i)=presg(i+1)-densg(i)*comi*consts.ggg;
+end
+
+
+for i=3:nx1-2
+     comi=-abs(rheight(i+1)-rheight(i));
+     %densg(i)=densg(i)-(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
+     densg(i)=(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
+end
+%update density
+% do ix_3=ixGlo3,ixGhi3
+%  do ix_2=ixGlo2,ixGhi2
+%   do ix_1=ixGlo1+2,ixGhi1-2
+%        
+%        w(ix_1,ix_2,ix_3,rho_)=-(1.D0/eqpar(grav1_))*(1.D0/(12.D0*(x(ix_1&
+%           +1,ix_2,ix_3,1)-x(ix_1,ix_2,ix_3,1))))*(w(ix_1+2,ix_2,ix_3,p_)&
+%           -8.D0*w(ix_1+1,ix_2,ix_3,p_)+8.D0*w(ix_1-1,ix_2,ix_3,&
+%           p_)                         -w(ix_1-2,ix_2,ix_3,p_))
+%                
+% 
+% 
+%      enddo
+%    enddo
+%  enddo 
+
+
+
+
+% !lower boundary
+% do ix_1=ixmin1+4,ixmin1+2,-1
+%         p_1=w(ix_1+2,ix_2,ix_3,p_)-8.d0*w(ix_1+1,ix_2,ix_3,p_)&
+%            +8.d0*w(ix_1-1,ix_2,ix_3,p_)
+%         p_2=w(ix_1,ix_2,ix_3,rho_)*eqpar(grav1_)
+%         w(ix_1-2,ix_2,ix_3,p_) = 12.d0*(x(ix_1,ix_2,ix_3,1)-x(ix_1&
+%            -1,ix_2,ix_3,1))*p_2+p_1
+for i=5:-1:3
+  p_1=presg(i+2)-8*presg(i+1)+8*presg(i-1);
+  p_2=-densg(i)*consts.ggg;
+  presg(i-2)= p_1+12.0*(rheight(i)-rheight(i-1))*p_2;
+end
+
+
+
+
+% !upper boundary
+% do ix_1=ixmax1-4,ixmax1-2
+%    do ix_2=ixmin2,ixmax2
+%       do ix_3=ixmin3,ixmax3
+%          
+%           p_1=w(ix_1-2,ix_2,ix_3,p_)-8.d0*w(ix_1-1,ix_2,ix_3,p_)+8.d0*w(ix_1&
+%              +1,ix_2,ix_3,p_)
+%           p_2=w(ix_1,ix_2,ix_3,rho_)*eqpar(grav1_)
+%           w(ix_1+2,ix_2,ix_3,p_) = -12.d0*(x(ix_1,ix_2,ix_3,1)-x(ix_1&
+%              -1,ix_2,ix_3,1))*p_2+p_1
+% 
+% !           p_1=w(ix_1-2,ix_2,p_)-8.d0*w(ix_1-1,ix_2,p_)+8.d0*w(ix_1+1,ix_2,p_)
+% !           p_2=w(ix_1,ix_2,rho_)*eqpar(grav1_)
+% !           w(ix_1+2,ix_2,p_) = -12.d0*(x(ix_1,ix_2,1)-x(ix_1-1,ix_2,1))*p_2+p_1
+% 
+%       enddo
+%    enddo
+% enddo
+for i=nx1-4:nx1-2
+  p_1=presg(i-2)-8*presg(i-1)+8*presg(i+1);
+  p_2=-densg(i)*consts.ggg;
+  presg(i+2)= p_1-12.0*(rheight(i)-rheight(i-1))*p_2;
+end
+
+presg2=presg;
+
+
+
+%compute enrgy using pressure
+% ! Calculate total energy from pressure, kinetic and magnetic energy
+% 
+% w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,e_)=w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,p_)/(eqpar(gamma_)-1)+half*((w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,rho_)+w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,rhob_))&
+%    *(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,v1_)**2+w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,v2_)**2+w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,v3_)**2)+((w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_))&
+%    **2+(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b2_))**2&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_))**2))&
+%    +((w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg1_))+(w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,b2_)*w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,bg2_))&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg3_)))
+for i=1:nx1
+    energg(i)=presg(i)/(consts.fgamma -1);
+end
+
+
+
+%e=p/(rho*(gamma-1.d0))+0.5d0*(bx*bx+bz*bz)
+% for i=4:nx1-3   
+%     tempg(i)=(tempg(i-3)+tempg(i-2)+tempg(i-1)+tempg(i+1)+tempg(i+2)+tempg(i+3))/6;
+% end
+
+
+
+
+
+
+
+disp('rebuilding array');
+      for i=1:nx1
+           for j=1:nx2
+               for k=1:nx3
+                   
+                   for iw=1:13
+                      simdata.w(i,j,k,iw)=0.0; 
+                   end
+                   simdata.w(i,j,k,10)=densg(i);  %density
+%                    presg(i)=tempg(i)*densg(i)*R/mu;
+%                    energ(i)=presg(i)./(densg(i)*(consts.fgamma-1));
+                   simdata.w(i,j,k,9)=energg(i);
+                   %simdata.w(i,j,k,9)=presg(i)./(densg(i)*(consts.fgamma-1));
+
+               end
+           end
+      end
+
+
 
 %writesac3D(newfilename, simparams, simgridinfo, simdata, 'ascii');
         
