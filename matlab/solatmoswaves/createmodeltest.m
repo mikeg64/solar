3f43386e3e9f8508d0d99d3a0c0b32fc2568f3ad
mikeg64 <m.griffiths@sheffield.ac.uk>
2017-12-10 10:41:31 +0000

    createmodeltest genertaes a valid atmosphere

--- matlab/solatmoswaves/createmodeltest.m
+++ matlab/solatmoswaves/createmodeltest.m
@@ -1,15 +1,28 @@
-newfilename='3D_128_2p5_2p5_12p5_asc.ini';
-
-
 simparams=sim_params;
 simgridinfo=sim_gridinfo;
 simdata=sim_data;
 
+
+
+
+%The config file 3D_128_spic_bin.ini is in the archive
+%pmodeini.tgz available at the following link
+%https://drive.google.com/open?id=0B-AKVl-pk6ziVUFqUzMycWJoODg
+
+
+
+newfilename='3D_128_4Mm_asc.ini';
+%newfilename='/data/cs1mkg/smaug/configs/3D_128_4Mm_bin.ini';
+
 consts.mu=0.6e0; %magnetic permeability
 consts.R=8.31e3
 consts.fgamma=1.66666667e0
-consts.ggg=274.0e0 % acceleration due to gravity on the sun
+consts.ggg=-274.0e0 % acceleration due to gravity on the sun
 consts.mu=4*pi/1.0e7
+
+ngx1=2;
+ngx2=2;
+ngx3=2;
 
   it=0;
        time=0;
@@ -17,60 +30,55 @@ consts.mu=4*pi/1.0e7
        neqpar=7;
        nw=13;
        nx1=128;
-        nx2=128;
+       nx2=128;
        nx3=128;
-     % nx2=4;
-     %  nx3=4;
 
-       
-        ng1=2;
-       ng2=2;
-       ng3=2;      
-       
-       
+
       gamma=1.666667;
+      adiab=1.0;
        eta=0;
        g1=-274;
        g2=0;
        g3=0;
        
        xmin=133333.33;
-       %xmin=199219.0;
-       %xmin=311111.0;
-       %ymin=1953.1;
-       ymin=39687.5;
-       %zmin=1953.1;
-       zmin=39687.5;
+       %xmin=2*266666;
+       ymin=1953.1;
+       zmin=1953.1;
        xmax=5955555.6e0;
-       %xmax=12.8496e6;
        ymax=4.0e6;
        zmax=4.0e6;
-       %ymax=2.559984e6;
-       %zmax=2.559984e6;
        
-       dx=(xmax-xmin)/(nx1-1-2*ng1);
-       dy=(ymax-ymin)/(nx2-1-2*ng2);
-       dz=(zmax-zmin)/(nx3-1-2*ng3);
+       dx=(xmax-xmin)/(nx1-2*ngx1);
+       dy=(ymax-ymin)/(nx2-2*ngx2);
+       dz=(zmax-zmin)/(nx3-2*ngx3);
        
        
        xx=zeros(nx1,nx2,nx3);
        yy=zeros(nx1,nx2,nx3);
        zz=zeros(nx1,nx2,nx3);
+
        rheight=zeros(1,nx1);
+       
        
        for i=1:nx1
            for j=1:nx2
                for k=1:nx3
-                   xx(i,j,k)=xmin-(ng1*dx)+dx*(i-1);
-                   yy(i,j,k)=ymin-(ng2*dy)+dy*(j-1);
-                   zz(i,j,k)=zmin-(ng3*dz)+dz*(k-1);
+                   xx(i,j,k)=(xmin-ngx1*dx)+(i+4)*dx;
+                   rheight(1,i)=(xmin-ngx1*dx)+i*dx;
+                   yy(i,j,k)=(ymin-ngx1*dx)+dy*j;
+                   zz(i,j,k)=(zmin-ngx1*dx)+dz*k;
                end
            end
        end
        
-       for i=1:nx1
-          rheight(1,i)=xmin+dx*(i-1); 
+       
+       for i=1:nx1+5
+           rheight(1,i)=(xmin-ngx1*dx)+i*dx;
+           
        end
+       
+       
        
        simdata.w=zeros(nx1,nx2,nx3,nw);
 
@@ -84,6 +92,7 @@ consts.mu=4*pi/1.0e7
 %         simparams.domain_left_edge=[0;0; 0.0];
 %         simparams.domain_right_edge=[0; 0; 0];
         simparams.eta=eta;
+        simparams.adiab=adiab;
 %        field_ordering=1;
         simparams.gamma=gamma;
         simparams.gravity0=g1;
@@ -123,77 +132,45 @@ temp = data(:,2);
 dens = data(:,3);
 pres = data(:,4);
 
-cs=sqrt(consts.fgamma.*pres./dens);
+cs=sqrt(consts.fgamma.*pres./dens); 
 
+% dens=1e12*dens;
+tempg=interp1(height,temp,(xmin-ngx1*dx):dx:(xmax+(ngx1+2)*dx));
+presg=interp1(height,pres,(xmin-ngx1*dx):dx:(xmax+(ngx1+2)*dx));
+densg=interp1(height,dens,(xmin-ngx1*dx):dx:(xmax+(ngx1+2)*dx));
 
+ninterp=size(tempg);
+nint=ninterp(2);
 
-%use fitting to extend atmos to 12.5Mm and 25Mm
+tempg(1,nint)=tempg(1,nint-4);
+tempg(1,nint-1)=tempg(1,nint-4);
+tempg(1,nint-2)=tempg(1,nint-4);
+tempg(1,nint-3)=tempg(1,nint-4);
+tempg(1,nint+1)=tempg(1,nint-4);
+tempg(1,nint+2)=tempg(1,nint-4);
 
-% tdens=dens(1:1269);
-% tpres=pres(1:1269);
-% theight=height(1:1269);
-% ttemp=temp(1:1269);
-
-
-%
-deltah=height(1)-height(2);
-maxheight=12.8496e6;
-% nvals=(maxheight-height(2048))/deltah;
-% nvals=4392;
-% for i=nvals:-1:1
-%     nheight(i,1)=height(2048,1)+(nvals-i+1)*deltah;
-% end
-
-% ndens(3614:4392,1)=dens(1270:2048);
-% ntemp(3614:4392,1)=temp(1270:2048);
-% npres(3614:4392,1)=pres(1270:2048);
-% 
-% 
-% %load the results from the fitting
-% load('dens_corona_fittedmodel.mat');
-% load('temp_corona_fittedmodel.mat');
-% load('pres_corona_fittedmodel.mat');
-% dens_corona=cfit(dens_corona_fittedmodel);
-% temp_corona=cfit(temp_corona_fittedmodel);
-% pres_corona=cfit(pres_corona_fittedmodel);
-% 
-% 
-% %compute values beyound transition region between 6.5Mm and 25Mm
-% %using data fitted with power law
-% for i=1:3613
-%     newh=nheight(i,1);
-% %using matlab fitting functions
-%      ndens(i,1)=dens_corona(newh);
-%      npres(i,1)=pres_corona(newh);
-%      ntemp(i,1)=temp_corona(newh);
-
-%old power law    
-%     ndens(i,1)=1.817e-7*newh.^(-0.667);
-%     npres(i,1)=6.717e-10*newh.^(1.219);
-%     ntemp(i,1)=2.669e-7*newh.^(1.886);
-%end
-
-
-
-%energg=interp1(nvals,nenerg,xmine:dxe:xmaxe);
-% tempg=interp1(nheight,ntemp,xmin:dx:xmax);
-% presg=interp1(nheight,npres,xmin:dx:xmax);
-% densg=interp1(nheight,ndens,xmin:dx:xmax);
-% energg=zeros(1,nx1);
-
-tempg=interp1(height,temp,xmin-(ng1*dx):dx:xmax+(ng1*dx));
-presg=interp1(height,pres,xmin-(ng1*dx):dx:xmax+(ng1*dx));
-densg=interp1(height,dens,xmin-(ng1*dx):dx:xmax+(ng1*dx));
-energg=zeros(1,nx1);
+presg(1,nint)=presg(1,nint-4);
+presg(1,nint-1)=presg(1,nint-4);
+presg(1,nint-2)=presg(1,nint-4);
+presg(1,nint-3)=presg(1,nint-4);
+densg(1,nint)=densg(1,nint-4);
+densg(1,nint-1)=densg(1,nint-4);
+densg(1,nint-2)=densg(1,nint-4);
+densg(1,nint-3)=densg(1,nint-4);
+densg(1,nint+1)=densg(1,nint-4);
+densg(1,nint+2)=densg(1,nint-4);
+%rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
+%set background density
+%set background energy
 
 
 %rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
 %set background density
 %set background energy
-mu=0.6d0;
+mu_thermal=0.6d0;
 R=8.31e3;
 
-%parrVALMc=rhoarrVALMc*TarrVALMc*R/mu
+%parrVALMc=rhoarrVALMc*TarrVALMc*R/mu_thermal
 %rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
 % p[*,*,*]=w[*,*,*,4]+w[*,*,*,8]
 % p[*,*,*]=p[*,*,*]-(w[*,*,*,1]^2.0+w[*,*,*,2]^2.0+w[*,*,*,3]^2.0)/ $
@@ -219,7 +196,7 @@ R=8.31e3;
 % 
 % ! 1.6Mm
 % 
-iniene=6840.d0*R*(2.3409724e-09)/mu/(consts.fgamma-1.0);
+iniene=6840.d0*R*(2.3409724e-09)/mu_thermal/(consts.fgamma-1.0);
 % 
 % !iniene=6840.d0*8.31e3*(2.2139002e-09)/0.6d0/(eqpar(gamma_)-1.0)
 % 
@@ -245,7 +222,7 @@ iniene=6840.d0*R*(2.3409724e-09)/mu/(consts.fgamma-1.0);
       for i=1:nx1
            for j=1:nx2
                for k=1:nx3
-                   simdata.w(i,j,k,10)=densg(i);  %density
+                   simdata.w(i,j,k,10)=densg(i+4);  %density
                    simdata.w(i,j,k,9)=iniene;  %energy
                                     
                end
@@ -275,7 +252,7 @@ iniene=6840.d0*R*(2.3409724e-09)/mu/(consts.fgamma-1.0);
 % p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=(eqpar(gamma_)&
 %    -one)*(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,e_)-p(ixmin1:ixmax1,&
 %    ixmin2:ixmax2,ixmin3:ixmax3))
-for i=1:nx1
+for i=1:nx1+5
     presg(i)=(consts.fgamma-1)*iniene;
 end
 
@@ -304,16 +281,16 @@ presg1=presg;
  
 
 
-for i=nx1-1:-1:1
+for i=nx1+5-1:-1:1
     comi=-abs(rheight(i+1)-rheight(i));
-    presg(i)=presg(i+1)-densg(i)*comi*consts.ggg;
+    presg(i)=presg(i+1)+densg(i)*comi*consts.ggg;
 end
 
 
-for i=3:nx1-2
+for i=3:nx1+5-2
      comi=-abs(rheight(i+1)-rheight(i));
      %densg(i)=densg(i)-(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
-     densg(i)=(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
+     densg(i)=-(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
 end
 %update density
 % do ix_3=ixGlo3,ixGhi3
@@ -341,11 +318,11 @@ end
 %         p_2=w(ix_1,ix_2,ix_3,rho_)*eqpar(grav1_)
 %         w(ix_1-2,ix_2,ix_3,p_) = 12.d0*(x(ix_1,ix_2,ix_3,1)-x(ix_1&
 %            -1,ix_2,ix_3,1))*p_2+p_1
-for i=5:-1:3
-  p_1=presg(i+2)-8*presg(i+1)+8*presg(i-1);
-  p_2=-densg(i)*consts.ggg;
-  presg(i-2)= p_1+12.0*(rheight(i)-rheight(i-1))*p_2;
-end
+ for i=5:-1:3
+   p_1=presg(i+2)+8*presg(i+1)-8*presg(i-1);
+   p_2=densg(i)*consts.ggg;
+   presg(i-2)= -p_1-12.0*(rheight(i)-rheight(i-1))*p_2;
+ end
 
 
 
@@ -368,11 +345,21 @@ end
 %       enddo
 %    enddo
 % enddo
-for i=nx1-4:nx1-2
+for i=nx1-4:nx1+5-2
   p_1=presg(i-2)-8*presg(i-1)+8*presg(i+1);
-  p_2=-densg(i)*consts.ggg;
+  p_2=densg(i)*consts.ggg;
   presg(i+2)= p_1-12.0*(rheight(i)-rheight(i-1))*p_2;
 end
+
+
+
+
+
+
+
+
+
+
 
 presg2=presg;
 
@@ -394,7 +381,7 @@ presg2=presg;
 %    ixmin3:ixmax3,b2_)*w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,bg2_))&
 %    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)*w(ixmin1:ixmax1,&
 %    ixmin2:ixmax2,ixmin3:ixmax3,bg3_)))
-for i=1:nx1
+for i=1:nx1+5
     energg(i)=presg(i)/(consts.fgamma -1);
 end
 
@@ -405,6 +392,12 @@ end
 %     tempg(i)=(tempg(i-3)+tempg(i-2)+tempg(i-1)+tempg(i+1)+tempg(i+2)+tempg(i+3))/6;
 % end
 
+
+
+
+
+
+
 disp('rebuilding array');
       for i=1:nx1
            for j=1:nx2
@@ -413,21 +406,19 @@ disp('rebuilding array');
                    for iw=1:13
                       simdata.w(i,j,k,iw)=0.0; 
                    end
-                   simdata.w(i,j,k,10)=densg(i);  %density
+                   simdata.w(i,j,k,10)=densg(i+4);  %density
 %                    presg(i)=tempg(i)*densg(i)*R/mu;
 %                    energ(i)=presg(i)./(densg(i)*(consts.fgamma-1));
-                   simdata.w(i,j,k,9)=energg(i);
+                   simdata.w(i,j,k,9)=energg(i+4);
+                   %simdata.w(i,j,k,9)=presg(i)./(densg(i)*(consts.fgamma-1));
+
                end
            end
       end
 
-       
-      
-      
-      
-    disp('writing array');  
-      
-      
-      
- writesac3D(newfilename, simparams, simgridinfo, simdata, 'ascii');
+disp('generate field');
+%
+%[simparams, simgridinfo, simdata]=generatefield(simparams, simgridinfo, simdata, 'fluxtube');
+
+% writesac3D(newfilename, simparams, simgridinfo, simdata, 'ascii');
         
