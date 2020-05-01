4a06d85b2d33b7eb31ceb48ea602a85449eadeda
mikeg64 <m.griffiths@sheffield.ac.uk>
2020-05-01 12:53:43 +0100

    working flux tube uniform  vertical field density 0.0002 in the tube and 0.0001 at the edges

---
+++ matlab/solatmoswaves/createmodel_uniformdensity_vertmagfluxtube.m
@@ -0,0 +1,448 @@
+newfilename='3D_128_2p5_2p5_12p5_asc_udvmft.ini'; %udvmft=uniform density vertical magnetic fluxt tube
+%extrapolating atmospheric model to greater than 6Mm
+%sets the correct minimum and maximum so that the ghost cells are also
+%included
+simparams=sim_params;
+simgridinfo=sim_gridinfo;
+simdata=sim_data;
+
+consts.mu=0.6e0; %magnetic permeability
+consts.R=8.31e3
+consts.fgamma=1.66666667e0
+consts.ggg= 0.0;  %-274.0e0 % acceleration due to gravity on the sun
+consts.mu=4*pi/1.0e7
+
+  it=0;
+       time=0;
+       ndim=3;
+       neqpar=7;
+       nw=13;
+       nx1=128;
+       nx2=128;
+       nx3=128;
+       
+       ng1=2;
+       ng2=2;
+       ng3=2;
+
+
+      gamma=1.666667;
+       eta=0;
+       g1=0;   %-274;
+       g2=0;
+       g3=0;
+      
+       %xmin=133333.33;
+       xmin=199219.0+6478.25;
+       %ymin=1953.1;
+       ymin=39687.5+1292.93;
+       %zmin=1953.1;
+       zmin=39687.5+1292.93;
+       %xmax=5955555.6e0;
+       xmax=12.8496e6;
+       %ymax=4.0e6;
+       %zmax=4.0e6;
+       ymax=2.559984e6;
+       zmax=2.559984e6;
+       
+       dx=(xmax-xmin)/(nx1-1-2*ng1);
+       dy=(ymax-ymin)/(nx2-1-2*ng2);
+       dz=(zmax-zmin)/(nx3-1-2*ng3);
+       
+       
+       xx=zeros(nx1,nx2,nx3);
+       yy=zeros(nx1,nx2,nx3);
+       zz=zeros(nx1,nx2,nx3);
+       
+       
+       for i=1:nx1
+           for j=1:nx2
+               for k=1:nx3
+                   rheight(i)=xmin-(ng1*dx)+dx*(i-1);
+                   xx(i,j,k)=xmin-(ng1*dx)+dx*(i-1);
+                   yy(i,j,k)=ymin-(ng2*dy)+dy*(j-1);
+                   zz(i,j,k)=zmin-(ng3*dz)+dz*(k-1);
+               end
+           end
+       end
+       
+       
+       
+       simdata.w=zeros(nx1,nx2,nx3,nw);
+
+
+
+
+        simparams.current_iteration=it;
+        simparams.current_time=time;
+        simparams.dimensionality=ndim;
+        simparams.domain_dimensions=[nx1;nx2;nx3];
+%         simparams.domain_left_edge=[0;0; 0.0];
+%         simparams.domain_right_edge=[0; 0; 0];
+        simparams.eta=eta;
+%        field_ordering=1;
+        simparams.gamma=gamma;
+        simparams.gravity0=g1;
+        simparams.gravity1=g2;
+        simparams.gravity2=g3;
+        
+        
+        
+        
+        simparams.domain_left_edge(1)=xx(1,1,1);
+        simparams.domain_left_edge(2)=yy(1,1,1);
+        simparams.domain_left_edge(3)=zz(1,1,1);
+        
+        simparams.domain_right_edge(1)=xx(nx1,1,1);
+        simparams.domain_right_edge(2)=yy(1,nx2,1);
+        simparams.domain_right_edge(3)=zz(1,1,nx3);
+
+        
+        simgridinfo.grid_dimensions(1)=nx1;
+        simgridinfo.grid_dimensions(2)=nx2;
+        simgridinfo.grid_dimensions(3)=nx3;
+        
+        %load atmosphere
+
+%       %% Import the data
+% data = xlsread('atmos.xls','VALMc_rho_2048_test');
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% %% Allocate imported array to column variable names
+% height = data(:,1);
+% temp = data(:,2);
+% dens = data(:,3);
+% pres = data(:,4);
+% 
+% cs=sqrt(consts.fgamma.*pres./dens);
+% 
+% 
+% 
+% %use fitting to extend atmos to 12.5Mm and 25Mm
+% 
+
+% densg(1:2048)=0.0001;
+% presg(1:2048)=7160.2;
+% tempg(1:2048)=5109.4;
+
+tempg=5109.4*ones(1,nx1);
+presg=7160.2*ones(1,nx1);
+densg=0.0001*ones(1,nx1);
+energ=zeros(1,nx1);
+
+% tdens=dens(1:1269);
+% tpres=pres(1:1269);
+% theight=height(1:1269);
+% ttemp=temp(1:1269);
+
+%dens(1:1269)=0.0001;
+
+%
+%deltah=height(1)-height(2);
+%maxheight=12.8496e6;
+% nvals=(maxheight-height(2048))/deltah;
+%nvals=4392;
+% for i=nvals:-1:1
+%     nheight(i,1)=height(2048,1)+(nvals-i+1)*deltah;
+% end
+% 
+% ndens(3614:4392,1)=dens(1270:2048);
+% ntemp(3614:4392,1)=temp(1270:2048);
+% npres(3614:4392,1)=pres(1270:2048);
+% 
+% 
+% %load the results from the fitting
+% load('dens_corona_fittedmodel.mat');
+% load('temp_corona_fittedmodel.mat');
+% load('pres_corona_fittedmodel.mat');
+% dens_corona=cfit(dens_corona_fittedmodel);
+% temp_corona=cfit(temp_corona_fittedmodel);
+% pres_corona=cfit(pres_corona_fittedmodel);
+% 
+% 
+% %compute values beyound transition region between 6.5Mm and 25Mm
+% %using data fitted with power law
+% for i=1:3613
+%     newh=nheight(i,1);
+% %using matlab fitting functions
+%      ndens(i,1)=dens_corona(newh);
+%      npres(i,1)=pres_corona(newh);
+%      ntemp(i,1)=temp_corona(newh);
+% 
+% %old power law    
+% %     ndens(i,1)=1.817e-7*newh.^(-0.667);
+% %     npres(i,1)=6.717e-10*newh.^(1.219);
+% %     ntemp(i,1)=2.669e-7*newh.^(1.886);
+% end
+% 
+% % for i=1:3613
+% %     nenerg(i)=npres(i)/(consts.fgamma -1);
+% % end
+% 
+% tempg=zeros(1,nx1);
+% presg=zeros(1,nx1);
+% densg=zeros(1,nx1);
+% energ=zeros(1,nx1);
+% 
+% 
+% % energg=interp1(nvals,nenerg,xmine:dxe:xmaxe);
+% tempg=interp1(nheight,ntemp,xmin-ng1*dx:dx:xmax+ng1*dx);
+% presg=interp1(nheight,npres,xmin-ng1*dx:dx:xmax+ng1*dx);
+% densg=interp1(nheight,ndens,xmin-ng1*dx:dx:xmax+ng1*dx);
+% 
+% ninterp=size(tempg);
+% nint=ninterp(2);
+% 
+% % tempg=tempgt(ng1:nx1-ng1);
+% % presg(ng1:nx1-ng1)=presgt;
+% % densg(ng1:nx1-ng1)=densgt;
+% % 
+
+
+
+% tempg(nx1)=tempg(nx1-2);
+% tempg(nx1-1)=tempg(nx1-2);
+% presg(nx1)=presg(nx1-2);
+% presg(nx1-1)=presg(nx1-2);
+% densg(nx1)=densg(nx1-2);
+% densg(nx1-1)=densg(nx1-2);
+% % 
+% tempg(1)=tempg(3);
+% tempg(2)=tempg(3);
+% presg(1)=presg(3);
+% presg(2)=presg(3);
+% densg(1)=densg(3);
+% densg(2)=densg(3);
+
+%rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
+%set background density
+%set background energy
+mu_thermal=0.6d0;
+R=8.31e3;
+
+%parrVALMc=rhoarrVALMc*TarrVALMc*R/mu
+%rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
+% p[*,*,*]=w[*,*,*,4]+w[*,*,*,8]
+% p[*,*,*]=p[*,*,*]-(w[*,*,*,1]^2.0+w[*,*,*,2]^2.0+w[*,*,*,3]^2.0)/ $
+%          (w[*,*,*,0]+w[*,*,*,9])/2.0
+% p[*,*,*]=p[*,*,*]-((w[*,*,*,5]+w[*,*,*,10])^2.0+(w[*,*,*,6]+w[*,*,*,11])^2.0 $
+%           +(w[*,*,*,7]+w[*,*,*,12])^2.0)/2.0
+% p[*,*,*]=(gamma-1.d0)*p[*,*,*]
+
+
+%compute correct pressure for gravitationally stratified atmosphere
+
+%compute initial energy (at photosphere or temperature minimum)
+%mu_thermal=0.6d0;
+%R=8.31e3;
+
+% temp*R*density/((mu_thermal))
+%parrVALMc=rhoarrVALMc*TarrVALMc*R/mu
+%iniene=6840.d0*8.31e3*(2.3409724e-09)/0.6d0/(eqpar(gamma_)-1.0)
+
+% !iniene=731191.34d0*8.31e3*(1.1806882e-11)/0.6d0/(eqpar(gamma_)-1.0)
+% 
+% !iniene=731191.34d0*8.31e3*(1.1790001e-11)/0.6d0/(eqpar(gamma_)-1.0)
+% 
+% ! 1.6Mm
+% 
+% !iniene=6840.d0*8.31e3*(2.3409724e-09)/0.6d0/(eqpar(gamma_)-1.0)
+%iniene=6840.d0*R*(2.3409724e-09)/mu_thermal/(consts.fgamma-1.0);
+
+%  ! 0.16Mm
+%iniene=5109.4d0*R*(0.0001)/mu_thermal/(consts.fgamma-1.0);
+% 
+% !iniene=6840.d0*8.31e3*(2.2139002e-09)/0.6d0/(eqpar(gamma_)-1.0)
+% 
+% !iniene=731191.34d0*8.31e3*(4.5335481e-12)/0.6d0/(eqpar(gamma_)-1.0)
+iniene=731191.34d0*R*(0.0001)/mu_thermal/(consts.fgamma-1.0);
+
+%inix and inirho read from height versus density dta from valiiic mcwhirter
+%data
+%  do ix_2=ixGlo2,ixGhi2
+%   do ix_3=ixGlo3,ixGhi3 
+% 
+%    x(ix_1,ix_2,ix_3,1)=inix !*1000.d0
+%    w(ix_1,ix_2,ix_3,rho_)=inirho
+%    w(ix_1,ix_2,ix_3,e_)=iniene
+%    w(ix_1,ix_2,ix_3,m1_)=0.0
+%    w(ix_1,ix_2,ix_3,m2_)=0.0
+%    w(ix_1,ix_2,ix_3,m3_)=0.0   
+% 
+%   enddo
+%  enddo
+
+
+      for i=1:nx1
+           for j=1:nx2
+               for k=1:nx3
+                   simdata.w(i,j,k,10)=densg(i);  %density
+                   simdata.w(i,j,k,9)=iniene;  %energy
+                                    
+               end
+           end
+      end
+
+
+
+% use energy to get pthermal
+
+% p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=half*( w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,m1_)**2+w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,m2_)**2+w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,m3_)&
+%    **2 )/(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,rho_)&
+%    +w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,rhob_))
+% 
+% p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=p(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3)+ half*( (w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_)&
+%    **2)+(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b2_)**2)&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)**2) )&
+%    +( (w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg1_))+(w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,b2_)*w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,bg2_))&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg3_)) )
+% 
+% p(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3)=(eqpar(gamma_)&
+%    -one)*(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,e_)-p(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3))
+
+for i=1:nx1
+    presg(i)=(consts.fgamma-1)*iniene;
+end
+
+presg1=presg;
+
+
+%compute correct pressure for gravitationally stratified atmosphere
+
+% do ix_3=ixGlo3,ixGhi3
+%  do ix_2=ixGlo2,ixGhi2
+%   do ix_1=ixGhi1-1,ixGlo1,-1 
+% 
+% comi=-abs(x(ix_1+1,ix_2,ix_3,1)-x(ix_1,ix_2,ix_3,1))
+% 
+% w(ix_1,ix_2,ix_3,p_)=w(ix_1+1,ix_2,ix_3,p_)+w(ix_1,ix_2,ix_3,rho_)*comi*1.d0&
+%    *eqpar(grav1_)
+% 
+% 
+% 
+%   enddo
+%  enddo
+% enddo
+
+% for i=nx1-1:-1:1
+%     comi=-abs(rheight(i+1)-rheight(i));
+%     presg(i)=presg(i+1)+densg(i)*comi*consts.ggg;
+% end
+
+
+% for i=3:nx1-2
+%      comi=-abs(rheight(i+1)-rheight(i));
+%      %densg(i)=densg(i)-(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
+%      densg(i)=-(1.0/consts.ggg)*(  (1.0/(12*(rheight(i+1)-rheight(i)))) *(presg(i+2)-8*presg(i+1)+8*presg(i-1)-presg(i-2))     );
+% end
+
+
+
+
+%update density
+% do ix_3=ixGlo3,ixGhi3
+%  do ix_2=ixGlo2,ixGhi2
+%   do ix_1=ixGlo1+2,ixGhi1-2
+%        
+%        w(ix_1,ix_2,ix_3,rho_)=-(1.D0/eqpar(grav1_))*(1.D0/(12.D0*(x(ix_1&
+%           +1,ix_2,ix_3,1)-x(ix_1,ix_2,ix_3,1))))*(w(ix_1+2,ix_2,ix_3,p_)&
+%           -8.D0*w(ix_1+1,ix_2,ix_3,p_)+8.D0*w(ix_1-1,ix_2,ix_3,&
+%           p_)                         -w(ix_1-2,ix_2,ix_3,p_))
+%                
+% 
+% 
+%      enddo
+%    enddo
+%  enddo 
+
+
+% !lower boundary
+% do ix_1=ixmin1+4,ixmin1+2,-1
+%         p_1=w(ix_1+2,ix_2,ix_3,p_)-8.d0*w(ix_1+1,ix_2,ix_3,p_)&
+%            +8.d0*w(ix_1-1,ix_2,ix_3,p_)
+%         p_2=w(ix_1,ix_2,ix_3,rho_)*eqpar(grav1_)
+%         w(ix_1-2,ix_2,ix_3,p_) = 12.d0*(x(ix_1,ix_2,ix_3,1)-x(ix_1&
+%            -1,ix_2,ix_3,1))*p_2+p_1
+
+% !upper boundary
+% do ix_1=ixmax1-4,ixmax1-2
+%    do ix_2=ixmin2,ixmax2
+%       do ix_3=ixmin3,ixmax3
+%          
+%           p_1=w(ix_1-2,ix_2,ix_3,p_)-8.d0*w(ix_1-1,ix_2,ix_3,p_)+8.d0*w(ix_1&
+%              +1,ix_2,ix_3,p_)
+%           p_2=w(ix_1,ix_2,ix_3,rho_)*eqpar(grav1_)
+%           w(ix_1+2,ix_2,ix_3,p_) = -12.d0*(x(ix_1,ix_2,ix_3,1)-x(ix_1&
+%              -1,ix_2,ix_3,1))*p_2+p_1
+% 
+% !           p_1=w(ix_1-2,ix_2,p_)-8.d0*w(ix_1-1,ix_2,p_)+8.d0*w(ix_1+1,ix_2,p_)
+% !           p_2=w(ix_1,ix_2,rho_)*eqpar(grav1_)
+% !           w(ix_1+2,ix_2,p_) = -12.d0*(x(ix_1,ix_2,1)-x(ix_1-1,ix_2,1))*p_2+p_1
+% 
+%       enddo
+%    enddo
+% enddo
+
+
+%compute enrgy using pressure
+% ! Calculate total energy from pressure, kinetic and magnetic energy
+% 
+% w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,e_)=w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,p_)/(eqpar(gamma_)-1)+half*((w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,rho_)+w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,rhob_))&
+%    *(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,v1_)**2+w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,v2_)**2+w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,v3_)**2)+((w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_))&
+%    **2+(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b2_))**2&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_))**2))&
+%    +((w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b1_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg1_))+(w(ixmin1:ixmax1,ixmin2:ixmax2,&
+%    ixmin3:ixmax3,b2_)*w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,bg2_))&
+%    +(w(ixmin1:ixmax1,ixmin2:ixmax2,ixmin3:ixmax3,b3_)*w(ixmin1:ixmax1,&
+%    ixmin2:ixmax2,ixmin3:ixmax3,bg3_)))
+
+
+
+
+%e=p/(rho*(gamma-1.d0))+0.5d0*(bx*bx+bz*bz)
+% for i=4:nx1-3   
+%     tempg(i)=(tempg(i-3)+tempg(i-2)+tempg(i-1)+tempg(i+1)+tempg(i+2)+tempg(i+3))/6;
+% end
+
+for i=1:nx1
+    energg(i)=presg(i)/(consts.fgamma -1);
+end
+
+      for i=1:nx1
+           for j=1:nx2
+               for k=1:nx3
+                   simdata.w(i,j,k,10)=densg(i);  %density
+%                    presg(i)=tempg(i)*densg(i)*R/mu;
+%                    energ(i)=presg(i)./(densg(i)*(consts.fgamma-1));
+                   simdata.w(i,j,k,9)=energg(i);
+               end
+           end
+      end
+
+       
+      
+      
+      
+      
+      
+      
+[simparams, simgridinfo, simdata]=generatefield_verttube_rhouni(simparams, simgridinfo, simdata, 'vertfieldtube');      
+writesac3D(newfilename, simparams, simgridinfo, simdata, 'ascii');
+        
---
+++ matlab/solatmoswaves/generatefield_verttube_rhouni.m
@@ -0,0 +1,470 @@
+function [simparams, simgridinfo, simdata]=generatefield_verttube_rhouni(simparams, simgridinfo, simdata, mode)
+
+%Generate magnetic field configuration
+%simple fluxtube using self similarity and hydrostatic pressure correction
+
+
+
+%Generate field
+% calculate hydrostatic pressure update
+
+
+%cases
+%1.  Vertical field
+%2. Horizontal field
+%3. Inclined (off vertical field)
+%4. Flux tube array
+
+if strcmp(mode,'vertfieldtube')
+    
+    
+    %tube width =100km
+    %footpoint intensity 1kG ala thin photospheric flux tubes
+    nb=1;
+    mb=1;
+    
+    nx1=simgridinfo.grid_dimensions(1);
+    nx2=simgridinfo.grid_dimensions(2);
+    nx3=simgridinfo.grid_dimensions(3);
+    
+    dx=-(simparams.domain_left_edge(1)-simparams.domain_right_edge(1))/simgridinfo.grid_dimensions(1);
+    dy=-(simparams.domain_left_edge(2)-simparams.domain_right_edge(2))/simgridinfo.grid_dimensions(2);
+    dz=-(simparams.domain_left_edge(3)-simparams.domain_right_edge(3))/simgridinfo.grid_dimensions(3);
+
+    
+    ymax=-(simparams.domain_left_edge(2)-simparams.domain_right_edge(2));
+    zmax=-(simparams.domain_left_edge(3)-simparams.domain_right_edge(3));
+    
+    bx=zeros(nx1,nx2,nx3);
+    by=zeros(nx1,nx2,nx3);
+    bz=zeros(nx1,nx2,nx3);
+    
+    rho=zeros(nx1,nx2,nx3);
+    
+    b0z=zeros(nx1,1);
+    
+    xf=zeros(nx1,nx2,nx3);
+    
+Bmax=0.15  ; %mag field Tesla
+%Bmin=0.0006d0  ; %mag field Tesla
+Bmin=0.0002  ; %mag field Tesla
+
+    
+    d_z=0.5; % width of Gaussian in Mm
+    z_shift= 0.0; % shift in Mm
+    A=0.45; % amplitude
+    scale=1.0e6;
+    b0z_top=0.08;
+   
+    f0=2.0e6; %tube opening factor
+
+    Ab0z=20.e0; % bz - amplitude
+    mu=4.0*pi*1.0e-7
+ 
+
+gamma=1.66666667;
+ggg=0.0; %-274.0;
+   
+
+xr=0.1e6;
+yr=0.1e6;
+
+R2=(xr.^2+yr.^2);
+
+A=R2/2;
+
+
+  
+    x=zeros(nx1,1);
+    y=zeros(nx2,1);
+    z=zeros(nx3,1);
+    
+    for j=1:nx2
+        y(j)=(simparams.domain_left_edge(2)+dy*(j-1))-(ymax/2);
+    end
+    for k=1:nx3
+        z(k)=(simparams.domain_left_edge(3)+dz*(k-1))-(zmax/2);
+    end
+    
+    for i=1:nx1
+        x(i)=simparams.domain_left_edge(1)+dx*(i-1);
+        %b0z(i)=(par4((x(i)/scale-z_shift),d_z,A)).^2;
+        %b0z(i)=(par3((x(i)/scale-z_shift),d_z,A));
+        %b0z(i)=par4((x(i)/scale-z_shift),d_z,A);
+        b0z(i)=Bmax;
+    end
+    
+    dyb=(simparams.domain_right_edge(2)-simparams.domain_left_edge(2))/nb;
+    dzb=(simparams.domain_right_edge(3)-simparams.domain_left_edge(3))/mb;
+    
+    
+    
+	bnmin=min(b0z);
+	bnmax=max(b0z);
+    
+    
+    for i=1:nx1
+	b0z(i)=((Bmax-Bmin)./(bnmax-bnmin)).*(b0z(i)-bnmin)+Bmin;
+    end
+    
+    %b0z=b0z./max(b0z);  
+    %b0z=Ab0z.*b0z+b0z_top;
+    
+    dbz=deriv1(b0z,x,1);
+
+
+	%y=y-(max(y)-min(y))/2 ;%+5000.d0
+	%z=z-(max(z)-min(z))/2 ;%+5000.d0
+
+  xold=x;
+  yold=y;
+  zold=z;
+    
+    
+ ib=0;
+ jb=0;
+    for ib=0:nb-1
+        for jb=0:mb-1
+
+
+
+
+
+            
+          %  if nb>1
+          %      ybp=simparams.domain_left_edge(2)+(ib+1)*nx2*dy*(simgridinfo.grid_dimensions(2)/((nb+1)))-nx2*dy*(simgridinfo.grid_dimensions(2)/(2*(nb+1)));
+          %  else
+          %      ybp=simparams.domain_left_edge(2)+(ib+1)*nx2*dy*(1/((nb+1)));                
+          %  end
+            
+          %  if mb>1
+          %      zbp=simparams.domain_left_edge(3)+(jb+1)*nx3*dz*(simgridinfo.grid_dimensions(3)/((mb+1)))-nx3*dz*(simgridinfo.grid_dimensions(3)/(2*(mb+1)));                
+          %  else
+          %      zbp=simparams.domain_left_edge(3)+(jb+1)*nx3*dz*(1/((mb+1)));
+          %  end
+            
+  
+          %  z=zold-max(z)/2.d0;
+          %  y=yold-max(y)/2.d0;
+
+            
+          %  z=zold-((jb-1)*dzb/2)-dzb;
+          %  y=yold-((ib-1)*dyb/2)-dyb;           
+            
+            
+            
+            
+for k=1:nx3
+for j=1:nx2
+for i=1:nx1
+
+%f=b0z(i)*sqrt((y(j)).^2+(z(k)).^2);
+
+%xf(i,j,k)=(par4(f,f0,0.5)).^2;
+
+
+f=(y(j).^2+z(k).^2)./R2;
+xf(i,j,k)=exp(-f);
+
+
+
+
+
+end
+end
+end
+xf=xf./max(max(max(xf)));
+b0zz=0.0001;
+rho0zz=0.0001;
+
+for k=1:nx3 
+for j=1:nx2
+for i=1:nx1
+
+% bz(i,j,k)=bz(i,j,k)+(b0z(i)/sqrt((x(j)-ybp).^2+(y(k)-zbp).^2)*xf(i,j,k));
+% bx(i,j,k)=bx(i,j,k)-(dbz(i)*(x(j)-ybp)/sqrt((x(j)-ybp).^2+(y(k)-zbp).^2)*xf(i,j,k));
+% by(i,j,k)=by(i,j,k)-dbz(i)*(y(k)-zbp)/sqrt((x(j)-ybp).^2+(y(k)-zbp).^2)*xf(i,j,k);
+%bz(i,j,k)=bz(i,j,k)+(b0z(i)/sqrt((x(j)).^2+(y(k)).^2)*xf(i,j,k));
+%bx(i,j,k)=bx(i,j,k)-(dbz(i)*(x(j))/sqrt((x(j)).^2+(y(k)).^2)*xf(i,j,k));
+%by(i,j,k)=by(i,j,k)-dbz(i)*(y(k))/sqrt((x(j)).^2+(y(k)).^2)*xf(i,j,k);
+
+
+bz(i,j,k)=bz(i,j,k)+b0zz.*xf(i,j,k);
+bx(i,j,k)=bx(i,j,k);
+by(i,j,k)=by(i,j,k);
+rho(i,j,k)=rho(i,j,k)+rho0zz.*xf(i,j,k);
+
+
+
+
+
+
+end
+end
+end           
+
+zold=z;
+yold=y;
+
+
+                       
+         end  %end of loop
+     end
+
+
+
+
+%     
+ end   %building fluxtube
+
+bb=(bx.^2+by.^2+bz.^2)/2/mu;
+
+
+% ************** convert to VAC magnetic field
+bz=bz./sqrt(mu);
+bx=bx./sqrt(mu);
+by=by./sqrt(mu);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%checked to here
+
+
+%compute magnetostatics pressure correction
+dbzdz=zeros(nx1,nx2,nx3);
+dbxdz=zeros(nx1,nx2,nx3);
+dbydz=zeros(nx1,nx2,nx3);
+
+dbzdx=zeros(nx1,nx2,nx3);
+dbxdx=zeros(nx1,nx2,nx3);
+dbydx=zeros(nx1,nx2,nx3);
+
+dbzdy=zeros(nx1,nx2,nx3);
+dbxdy=zeros(nx1,nx2,nx3);
+dbydy=zeros(nx1,nx2,nx3);
+
+
+Bvarix=zeros(nx1,nx2,nx3);
+Bvariy=zeros(nx1,nx2,nx3);
+Bvari=zeros(nx1,nx2,nx3);
+
+dpdx=zeros(nx1,nx2,nx3);
+
+rho1=zeros(nx1,nx2,nx3);
+
+Bvaridz=zeros(nx1,nx2,nx3);
+Bvaridz1=zeros(nx1,nx2,nx3);
+dBvardz=zeros(nx1,nx2,nx3);
+br=zeros(nx1,nx2,nx3);
+
+bxby=zeros(nx1,nx2,nx3);
+dbxbydy=zeros(nx1,nx2,nx3);
+bxbz=zeros(nx1,nx2,nx3);
+dbxbzdz=zeros(nx1,nx2,nx3);
+bxby=zeros(nx1,nx2,nx3);
+bybz=zeros(nx1,nx2,nx3);
+dbybzdy=zeros(nx1,nx2,nx3);
+
+for k=1:nx3
+for j=1:nx2
+ dbzdz(:,j,k)=deriv1(bz(:,j,k),x,1);
+% dbxdz(:,j,k)=deriv1(bx(:,j,k),x);
+% dbydz(:,j,k)=deriv1(by(:,j,k),x);
+end
+end
+
+
+for k=1:nx3
+for i=1:nx1
+% dbzdx(i,:,k)=deriv1(bz(i,:,k),y);
+ dbxdx(i,:,k)=deriv1(bx(i,:,k),y,2);
+% dbydx(i,:,k)=deriv1(by(i,:,k),y);
+end
+end
+
+
+for j=1:nx2
+for i=1:nx1
+% dbzdy(i,j,:)=deriv1(bz(i,j,:),z);
+% dbxdy(i,j,:)=deriv1(bx(i,j,:),z);
+ dbydy(i,j,:)=deriv1(by(i,j,:),z,3); 
+end
+end
+
+divb=dbzdz+dbxdx+dbydy;
+
+%bxby=bx.*by;
+
+%%define matlab code here
+%check b_field_vertical_tube.pro
+bxby=bx.*by;
+
+for j=1:nx2
+for i=1:nx1
+ dbxbydy(i,j,:)=deriv1(bxby(i,j,:),z,3);
+end
+end
+
+
+%print,'dBxBydy'
+
+
+bxbz=bx.*bz;
+
+for j=1:nx2
+for i=1:nx1
+ dbxbzdz(:,j,k)=deriv1(bxbz(:,j,k),x,1);
+end
+end
+
+
+
+
+%;print,'dBxBzdz'
+
+
+bxby=bx.*by;
+
+for i=1:nx1
+for k=1:nx3
+ dbxbydx(i,:,k)=deriv1(bxby(i,:,k),y,2);
+end
+end
+
+%print,'dBxBydx'
+
+
+
+%bybz=dblarr(n1,n2,n3)
+%dbybzdz=dblarr(n1,n2,n3)
+bybz=by.*bz;
+
+for j=1:nx2
+for k=1:nx3
+ dbybzdz(:,j,k)=deriv1(bybz(:,j,k),x,1);
+end
+end
+
+%************* BEGIN INTEGRATION ****************************
+F=dbxbydy+dbxbzdz;
+G=dbxbydx+dbybzdz;
+
+
+
+
+
+for i=1:nx1
+
+for kx=1:nx3
+  for jx=1:nx2
+   sum=inte(reshape(F(i,1:jx,kx),[jx,1]),y(2)-y(1)); 
+  Bvarix(i,jx,kx)=sum;
+ end
+end
+
+for jy=1:nx2
+  for ky=1:nx3
+   sum=inte(reshape(G(i,jy,1:ky),[ky,1]),z(2)-z(1)); 
+  Bvariy(i,jy,ky)=sum;
+ end
+end
+display(  i);
+
+end
+
+%************* END INTEGRATION ****************************
+
+Bvari=((Bvarix+Bvariy)/2)-(bz.^2)/2;
+
+
+
+for j=1:nx2
+for k=1:nx3
+ dpdz(:,j,k)=deriv1(Bvari(:,j,k),z,1);
+end
+end
+
+bxbybz=(bx.*bx+by.*by-bz.*bz)/2;
+
+for j=1:nx2
+for k=1:nx3
+ dbxbybzdz(:,j,k)=deriv1(bxbybz(:,j,k),z,1);
+end
+end
+
+
+bxbz=bx.*bz;
+
+for i=1:nx1
+for k=1:nx3
+ dbxbzdx(i,:,k)=deriv1(bxbz(i,:,k),x,2);
+end
+end
+
+
+% bybz=dblarr(n1,n2,n3)
+% dbybzdy=dblarr(n1,n2,n3)
+bybz=by.*bz;
+
+for i=1:nx1
+for j=1:nx2
+ dbybzdy(i,j,:)=deriv1(bybz(i,j,:),y,3);
+end
+end
+
+
+%for i=1:nx1
+%for j=1:nx2
+%for k=1:nx3
+% rho1(i,j,k)=(dbxbybzdz(i,j,k)-dbxbzdx(i,j,k)-  dbybzdy(i,j,k)+dpdz(i,j,k))./ggg;
+%end
+%end
+%end
+
+
+%rho1=(dbxbybzdz-dbxbzdx-  dbybzdy+dpdz)./ggg;
+
+rho1=simdata.w(:,:,:,1)+rho+simdata.w(:,:,:,10);
+p=Bvari+(simdata.w(:,:,:,5)+simdata.w(:,:,:,9))*(gamma-1.0);
+
+
+%lower boundary
+
+% for ix_1=4:-1:2
+%   for ix_2=1:nx2
+%   for ix_3=1:nx3  
+%          p_2=rho1(ix_1,ix_2,ix_3)*ggg;
+%          p(ix_1-1,ix_2,ix_3) = (z(2)-z(1))*p_2+p(ix_1,ix_2,ix_3);
+%   end  
+%   end
+%  end
+
+
+%upper boundary
+
+% for ix_1=nx1-2:nx1-1
+%    for ix_2=1:nx2
+%    for ix_3=1:nx3   
+%            p_2=rho1(ix_1,ix_2,ix_3)*ggg;
+%            p(ix_1+1,ix_2,ix_3) = -(z(2)-z(1))*p_2+p(ix_1,ix_2,ix_3);
+%    end	   
+%    end
+% end
+
+
+%rho, mom1, mom2, mom3, energy, b1, b2, b3,energyb,rhob,b1b,b2b,b3b
+%set background density
+%set background energy
+
+
+
+
+
+
+%update the background energy and magnetic fields
+simdata.w(:,:,:,10)=rho1;
+simdata.w(:,:,:,9)=p./((gamma-1.0))+0.5*(bx.*bx+bz.*bz+by.*by);
+simdata.w(:,:,:,11)=bz;
+simdata.w(:,:,:,12)=bx;
+simdata.w(:,:,:,13)=by;
+
+
